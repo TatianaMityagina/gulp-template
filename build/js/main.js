fae241e98b35703b989e55ac4f12474c30eba831
TatianaMityagina <394999325@mail.ru>
2019-09-29 21:07:45 +0300

    Для проверки готовая версия 1.0

--- build/js/main.js
+++ build/js/main.js
@@ -54,15 +54,13 @@ setupClose.addEventListener('keydown', function (evt) {
 
 var anchors = document.querySelectorAll('a[href*="#"]')
 
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (evt) {
-    evt.preventDefault()
+anchors.addEventListener('click', function (evt) {
+  evt.preventDefault();
 
-    var blockID = anchor.getAttribute('href').substr(1)
+  var blockID = anchors.getAttribute('href').substr(1);
 
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-}
+  document.getElementById(blockID).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+});
